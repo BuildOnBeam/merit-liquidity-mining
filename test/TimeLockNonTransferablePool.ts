43bb9f050a178150c74c58b08ce86a184cf31cfa
Mick de Graaf <mick@mickdegraaf.nl>
2021-10-04 17:09:59 +0200

    TimeLockNonTransferable tests

--- test/TimeLockNonTransferablePool.ts
+++ test/TimeLockNonTransferablePool.ts
@@ -1,13 +1,103 @@
+import { parseEther } from "@ethersproject/units";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+import { BigNumber, constants } from "ethers";
 import hre from "hardhat";
+import { TestToken__factory, TimeLockNonTransferablePool__factory } from "../typechain";
+import { TestToken } from "../typechain";
+import { TimeLockNonTransferablePool } from "../typechain/TimeLockNonTransferablePool";
 import TimeTraveler from "../utils/TimeTraveler";
 
-describe("TimeLockNonTransferablePool", function () {
+const ESCROW_DURATION = 60 * 60 * 24 * 365;
+const ESCROW_PORTION = parseEther("0.77");
+const MAX_BONUS = parseEther("1");
+const MAX_LOCK_DURATION = 60 * 60 * 24 * 365;
+const INITIAL_MINT = parseEther("1000000");
+const DEPOSIT_AMOUNT = parseEther("1000");
+
+describe("TimeLockPool", function () {
+
+    let deployer: SignerWithAddress;
+    let account1: SignerWithAddress;
+    let account2: SignerWithAddress;
+    let account3: SignerWithAddress;
+    let account4: SignerWithAddress;
+    let signers: SignerWithAddress[];
+
+    let timeLockPool: TimeLockNonTransferablePool;
+    let escrowPool: TimeLockNonTransferablePool;
+    let depositToken: TestToken;
+    let rewardToken: TestToken;
+    
+    const timeTraveler = new TimeTraveler(hre.network.provider);
+
+    before(async() => {
+        [
+            deployer,
+            account1,
+            account2,
+            account3,
+            account4,
+            ...signers
+        ] = await hre.ethers.getSigners();
+
+        const testTokenFactory = await new TestToken__factory(deployer);
+
+        depositToken = await testTokenFactory.deploy("DPST", "Deposit Token");
+        rewardToken = await testTokenFactory.deploy("RWRD", "Reward Token");
+
+        await depositToken.mint(account1.address, INITIAL_MINT);
+        await rewardToken.mint(account1.address, INITIAL_MINT);
+
+        const timeLockPoolFactory = new TimeLockNonTransferablePool__factory(deployer);
+        
+        escrowPool = await timeLockPoolFactory.deploy(
+            "ESCROW",
+            "ESCRW",
+            rewardToken.address,
+            constants.AddressZero,
+            constants.AddressZero,
+            0,
+            0,
+            0,
+            ESCROW_DURATION
+        );
+
+        timeLockPool = await timeLockPoolFactory.deploy(
+            "Staking Pool",
+            "STK",
+            depositToken.address,
+            rewardToken.address,
+            escrowPool.address,
+            ESCROW_PORTION,
+            ESCROW_DURATION,
+            MAX_BONUS,
+            MAX_LOCK_DURATION
+        );
+
+        
+        // connect account1 to all contracts
+        timeLockPool = timeLockPool.connect(account1);
+        escrowPool = escrowPool.connect(account1);
+        depositToken = depositToken.connect(account1);
+        rewardToken = rewardToken.connect(account1);
+        
+        await depositToken.approve(timeLockPool.address, constants.MaxUint256);
+        await timeLockPool.deposit(DEPOSIT_AMOUNT, constants.MaxUint256, account1.address);
+
+        await timeTraveler.snapshot();
+    })
+
+    beforeEach(async() => {
+        await timeTraveler.revertSnapshot();
+    })
+
     it("transfer", async() => {
-
+        await expect(timeLockPool.transfer(account3.address, DEPOSIT_AMOUNT)).to.be.revertedWith("NON_TRANSFERABLE");
     });
-    it("transferFrom", async() => {
 
+    it("transferFrom", async() => {
+        await expect(timeLockPool.transferFrom(account1.address, account3.address, DEPOSIT_AMOUNT)).to.be.revertedWith("NON_TRANSFERABLE");
     });
 });
